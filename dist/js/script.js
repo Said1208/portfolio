2fb350cb7b52bb35afad7936be027e71c162b510
grishaand1@gmail.com <saidikram999@gmail.com>
2021-03-05 21:21:33 +0600

    second

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,3 +16,17 @@ const counters = document.querySelectorAll('.skills__ratings-counter'),
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+$(window).scroll(function () {
+    if($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+$("a[href^='#up']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
